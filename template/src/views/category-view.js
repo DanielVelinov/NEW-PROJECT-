54eace76cc4dc2476e8fea1e54b11b9eae250272
Ivo Dimitrov <lautaspartan@gmail.com>
2024-07-11 14:05:25 +0300

    fix toCategoriesView

--- template/src/views/category-view.js
+++ template/src/views/category-view.js
@@ -1,23 +1,13 @@
-import { apiKey } from "../common/constants.js";
-
-export async function toCategoriesView() {
-    const url = `https://api.giphy.com/v1/gifs/categories?api_key=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const categoriesContainer = document.createElement('div');
-    categoriesContainer.id = 'categories';
-    categoriesContainer.innerHTML = `
+export const toCategoriesView = (categories) => `
+    <div id="categories">
         <h1>Categories</h1>
         <div class="content">
-            ${data.data.map(category => `
+            ${categories.map(category => `
                 <div class="category">
                     <img src="${category.gif.images.fixed_height.url}" alt="${category.name}">
                     <p>${category.name}</p>
                 </div>
             `).join('')}
         </div>
-    `;
-
-    return categoriesContainer.outerHTML;
-}
+    </div>
+`;
