eb00e70f2b5f15bc05c736d461854ce91eead626
Ivo Dimitrov <lautaspartan@gmail.com>
2024-07-11 14:28:16 +0300

    fix

--- template/src/events/search-events.js
+++ template/src/events/search-events.js
@@ -1,13 +1,14 @@
 import { toGIFsView } from "../views/movie-views.js";
 import { CONTAINER_SELECTOR } from "../common/constants.js";
 import { q } from "./helpers.js";
+import { loadSearchGifs } from "../requests/request-service.js";
 
 export const renderSearchItems = async (searchTerm) => {
   const searchInput = document.getElementById('search');
   const query = searchInput.value.trim();
 
   if (query) {
-    const gifs = await loadSearchMovies(query);
+    const gifs = await loadSearchGifs(query);
     q(CONTAINER_SELECTOR).innerHTML = toGIFsView(gifs.data);
     searchInput.value = '';
   }
