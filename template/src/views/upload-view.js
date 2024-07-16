64f2980eb94fc62ad14af88cec69e0decc81edca
Ivo Dimitrov <lautaspartan@gmail.com>
2024-07-16 21:55:31 +0300

    upload

--- template/src/views/upload-view.js
+++ template/src/views/upload-view.js
@@ -3,9 +3,9 @@ export const toUploadView = () => `
     <h2>Add something new...</h2>
     <label for="images" class="drop-container" id="dropcontainer">
   <span class="drop-title">Upload gif here </span>
-  <input type="file" id="images" accept="*" required></input>
+  <input type="file" id="gif-file" accept="*" required></input>
   </label>
-    <button id="upload-button" class="upload-button">Finish</button>
+    <button id="upload-button" class="upload-button">Upload</button>
     <div id="upload-status"></div>
   </div>
 `;
