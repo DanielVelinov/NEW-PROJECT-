b835d56c6abb52ad6fa7fa98fc370086f00608e7
mivelgroup@gmail.com <mivelgroup@gmail.com>
2024-07-16 21:44:20 +0300

    upload css update

--- template/src/views/upload-view.js
+++ template/src/views/upload-view.js
@@ -1,8 +1,11 @@
 export const toUploadView = () => `
   <div id="upload-section">
-    <h2>Upload a GIF</h2>
-    <input type="file" id="gif-file" accept="image/gif">
-    <button id="upload-button" class="upload-button">Upload</button>
+    <h2>Add something new...</h2>
+    <label for="images" class="drop-container" id="dropcontainer">
+  <span class="drop-title">Upload gif here </span>
+  <input type="file" id="images" accept="*" required></input>
+  </label>
+    <button id="upload-button" class="upload-button">Finish</button>
     <div id="upload-status"></div>
   </div>
 `;
