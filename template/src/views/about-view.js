266b05e5de8873a84514b075ed04754b58547f02
mivelgroup@gmail.com <mivelgroup@gmail.com>
2024-07-16 19:48:51 +0300

    about

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -3,13 +3,19 @@ import { q } from "../events/helpers.js";
 
 export const toAboutView = () => {
   q(CONTAINER_SELECTOR).innerHTML = `
-<div id="about">
-  <div class="content">
-    <h1>About the app</h1>
-    <h2>Authors: Telerik Academy</h2>
-    <h2>Date: 2021</h2>
-  </div>
-</div>
-`
+    <div id="about">
+      <div class="content">
+        <h1>About the app</h1>
+        <h2>Created by: Daniel Velinov, Ivo Dimitrov, and Stoyan Radichev.</h2>
+        <h3>Technologies used</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>HTML & CSS</li>
+          <li>Node.js</li>
+        </ul>
+        <h4>Contact US</h4>
+        <p><a href="www.telerikacademy.com">www.telerikacademy.com</a></p>
+      </div>
+    </div>
+  `;
 };
-// TODO
--- template/src/views/upload-view.js
+++ template/src/views/upload-view.js
@@ -2,7 +2,7 @@ export const toUploadView = () => `
   <div id="upload-section">
     <h2>Upload a GIF</h2>
     <input type="file" id="gif-file" accept="image/gif">
-    <button id="upload-button">Upload</button>
+    <button id="upload-button" class="upload-button">Upload</button>
     <div id="upload-status"></div>
   </div>
 `;
