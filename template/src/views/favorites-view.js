d9633e78efe0a063decff953c2090855bd7ac0aa
mivelgroup@gmail.com <mivelgroup@gmail.com>
2024-07-16 13:39:07 +0300

    favorites view

--- template/src/views/favorites-view.js
+++ template/src/views/favorites-view.js
@@ -1,27 +1,28 @@
+// views/favorites-view.js
 import { FULL_HEART, EMPTY_HEART } from "../common/constants.js";
 import { renderFavoriteStatus } from "../events/helpers.js";
 
 export const toFavoritesView = (favorites) => {
     if (favorites.length === 0) {
-        return ' <h1>No favorites yet</h1>';
+        return '<h1>No favorites yet</h1>';
     } else {
         return `
-      <h1></h1>
-      <div id="trending">
-        ${favorites.map(gif => `
-          <div class="content">
-          <div class="gif">
+      <h1>Favorites</h1>
+      <div id="favorites">
+        <div class="content">
+          ${favorites.map(gif => `
+            <div class="gif">
             <div class="separate-gifs">
-              <img src="${gif.images?.downsized_medium.url}" alt="${gif.title}">
+              <img src="${gif.images.fixed_height.url}" alt="${gif.title}">
               <div class="overlay"></div>
               <div class="buttons">
                 <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
-                <button class="remove-from-favorites" data-gif-id="${gif.id}">${FULL_HEART}</button>
+                <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
               </div>
               </div>
             </div>
-          </div>
-        `).join('\n')}
+          `).join('')}
+        </div>
       </div>
     `;
     }
